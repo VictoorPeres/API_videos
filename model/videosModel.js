b1413a5974d7c701db5297616dc027d4d76ed7fe
Victor Alexandre <alexanvictor@gmail.com>
2023-01-20 20:18:20 -0400

    Criação do método de listar por ID

--- model/videosModel.js
+++ model/videosModel.js
@@ -10,5 +10,18 @@ class Videos{
             }
         })
     }
+    listId(cd_video, res){
+        const sql = `SELECT * FROM tbVideos WHERE cd_video = ${cd_video}`;
+        
+        connection.query(sql, (error, result) => {
+            const resultObj = result[0];
+            
+            if(error){
+                res.status(400).json(error);
+            }else{
+                res.status(200).json(resultObj);
+            }
+        })
+    }
 }
 module.exports = new Videos;
