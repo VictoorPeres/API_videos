fd5dc50c8dcc0178e031bc5c9a73859c2945a368
Victor Alexandre <alexanvictor@gmail.com>
2023-01-20 20:18:51 -0400

    Criação da rota que retorna a pesquisa por ID

--- controllers/videosController.js
+++ controllers/videosController.js
@@ -4,5 +4,10 @@ module.exports = app => {
     app.get('/videos', (req, res) => {
     Videos.list(res);        
     });
+
+    app.get('/videos/:id', (req, res) => {
+        const cd_video = parseInt(req.params.id);
+        Videos.listId(cd_video, res);
+    })
 }
 
