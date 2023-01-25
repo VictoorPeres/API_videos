b45e05ee3adce9e9fe6175832a3ed5d355a2a924
Victor Alexandre <alexanvictor@gmail.com>
2023-01-24 22:29:40 -0400

    Criado o método que realizar a inserção de dados no banco

--- model/categoriasModel.js
+++ model/categoriasModel.js
@@ -25,6 +25,38 @@ class Categoria{
             }
         })
     }
+    create(dadosCategoria, res){
+        const sql = 'INSERT INTO tbCategorias SET ?';
+        const {titulo, cor} = dadosCategoria;
+        const validaTitulo = titulo.length >= 3;
+        const validaCor = cor.length == 7;
+
+        const validacoes = [
+            {
+                nome: 'titulo',
+                valida: validaTitulo,
+                mensagem: 'O título deve ter três ou mais caracteres'
+            },
+            {
+                nome: 'cor',
+                valida: validaCor,
+                mensagem: 'A cor deve ter sete caracteres'
+            }
+        ];
+        const erros = validacoes.filter(campo => !campo.valida);
+       // const errosExistem = erros.length;
+        if(erros.length){
+            res.status(400).json(erros);
+        }else{
+            connection.query(sql, dadosCategoria, (error, result) => {
+                if(error){
+                    res.status(400).json(erro);
+                }else{
+                    res.status(200).json(dadosCategoria);
+                }
+            })
+        }
+    }
 }
 
 module.exports = new Categoria;
