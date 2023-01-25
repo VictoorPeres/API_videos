59ef5f904393a28c0dab0119742f7d3527447adf
Victor Alexandre <alexanvictor@gmail.com>
2023-01-24 22:05:58 -0400

    Criação da rota get que lista as categorias por ID

--- controllers/categoriasController.js
+++ controllers/categoriasController.js
@@ -3,4 +3,8 @@ module.exports = app => {
     app.get('/categorias', (req, res) => {
         Categoria.list(res);
     });
+    app.get('/categoria/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Categoria.listId(id, res);
+    });
 }
