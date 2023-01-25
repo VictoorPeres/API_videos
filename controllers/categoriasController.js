89a3b00ebce6bb4001750f3367a71713f12c7de0
Victor Alexandre <alexanvictor@gmail.com>
2023-01-24 22:53:44 -0400

    Criada a rota para PUT para edição de categorias por cd_categoria

--- controllers/categoriasController.js
+++ controllers/categoriasController.js
@@ -4,11 +4,16 @@ module.exports = app => {
         Categoria.list(res);
     });
     app.get('/categoria/:id', (req, res) => {
-        const id = parseInt(req.params.id);
+        const cd_categoria = parseInt(req.params.cd_categoria);
         Categoria.listId(id, res);
     });
     app.post('/categoria', (req, res) => {
         const dadosCategoria = req.body;
         Categoria.create(dadosCategoria, res);
+    })
+    app.put('/categoria/:id', (req, res) => {
+        const cd_categoria = parseInt(req.params.id);
+        const dadosCategoria = req.body;
+        Categoria.update(cd_categoria, dadosCategoria, res);
     })
 }
