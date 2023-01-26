14c4440f9840f3acbf5a982ca0038f6e727c8bb4
Victor Alexandre <alexanvictor@gmail.com>
2023-01-25 22:28:04 -0400

    Criação do método delete que quando chamado realizara a exclusão no banco

--- model/categoriasModel.js
+++ model/categoriasModel.js
@@ -67,6 +67,18 @@ class Categoria{
             }
         })
     }
+    delete(cd_categoria, res){
+        const sql = 'DELETE FROM tbCategorias WHERE cd_categoria = ?';
+        connection.query(sql, cd_categoria, (error, result) => {
+            if(error){
+                res.status(400).json(error)
+            }else if(result.affectedRows === 1){
+                res.status(200).json('A categoria foi excluída.');
+            }else{
+                res.status(400).json('Categoria não encontrada.');
+            }
+        });
+    }
 }
 
 module.exports = new Categoria;
