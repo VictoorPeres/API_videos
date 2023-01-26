e9479d5e4f4b77aee19d6f22b4874dda520cedec
Victor Alexandre <alexanvictor@gmail.com>
2023-01-25 22:27:13 -0400

    Criação da rota de exclusão que chama o metodo responsavel por excluir as categorias por id

--- controllers/categoriasController.js
+++ controllers/categoriasController.js
@@ -4,16 +4,20 @@ module.exports = app => {
         Categoria.list(res);
     });
     app.get('/categoria/:id', (req, res) => {
-        const cd_categoria = parseInt(req.params.cd_categoria);
-        Categoria.listId(id, res);
+        const cd_categoria = parseInt(req.params.id);
+        Categoria.listId(cd_categoria, res);
     });
     app.post('/categoria', (req, res) => {
         const dadosCategoria = req.body;
         Categoria.create(dadosCategoria, res);
-    })
+    });
     app.put('/categoria/:id', (req, res) => {
         const cd_categoria = parseInt(req.params.id);
         const dadosCategoria = req.body;
         Categoria.update(cd_categoria, dadosCategoria, res);
+    });
+    app.delete('/categoria/:id', (req, res) => {
+        const cd_categoria = parseInt(req.params.id);
+        Categoria.delete(cd_categoria, res);
     })
 }
